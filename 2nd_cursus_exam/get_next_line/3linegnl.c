6741289582a6a1639864589364968a92c8afeb2c
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-07-28 08:15:20 -0400

    ok

--- 2nd_cursus_exam/get_next_line/3linegnl.c
+++ 2nd_cursus_exam/get_next_line/3linegnl.c
@@ -30,3 +30,34 @@ int main(int argc, char **argv)
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char *get_next_line(int fd)
+{
+	char *s = malloc(100000), *c = s;
+	while(read(fd, c, 1) > 0 && *c++ != '\n');
+	return  c > s  ? (c = 0, s) : (free(s), NULL);
+}
