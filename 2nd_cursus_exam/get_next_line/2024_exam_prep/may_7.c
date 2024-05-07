0e7da91d9c565008560e164f9c31d762d7ec2b4b
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2024-05-07 09:19:17 -0400

    may 7 morning

--- 2nd_cursus_exam/get_next_line/2024_exam_prep/may_7.c
+++ 2nd_cursus_exam/get_next_line/2024_exam_prep/may_7.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 
 #ifndef BUFFER_SIZE
@@ -16,7 +17,16 @@ int ft_strlen(char *str)
 		i++;
 	return(i);
 }
-
+char *ft_strchr(char *str, char c)
+{
+	while(*str)
+	{
+		if (*str == c)
+			return (str);
+		str++;
+	}
+	return(NULL);
+}
 char *ft_strjoin(char* s1, char* s2)
 {
 	int s1_len = ft_strlen(s1);
@@ -29,7 +39,7 @@ char *ft_strjoin(char* s1, char* s2)
 	//copy s2
 	for(int i = 0; s2[i];i++)
 		rtn[ft_strlen(s1)+i] = s2[i];
-	rtn[s1_len+s2_len] = '\0';
+	rtn[s1_len + s2_len + 1]  = '\0';
 	//free s1
 	if (s1)
 		free(s1);
@@ -58,23 +68,53 @@ char *ft_substr(char *str, int start, int lenght)
 char *get_next_line(int fd)
 {
 	char buffer[BUFFER_SIZE +1];
-	static char *rtn;
+	static char *line;
 	char *new_line;
+	int count;
+	char *rtn;
 
+	if (!line)
+		line = malloc(1);
 	if (fd < 0)
 		return(NULL);
 	while(1)
 	{
-
+		if (ft_strchr(line, '\n'))
+			break;
+		count = read(fd, buffer, BUFFER_SIZE);
+		if (count != BUFFER_SIZE)
+		{
+			buffer[count] = '\0';
+			rtn = ft_substr(buffer, 0, ft_strlen(buffer));
+			free(line);
+			return(rtn);
+		}
+		buffer[count] = '\0';
+		line = ft_strjoin(line, buffer);
 	}
+	new_line = ft_strchr(line, '\n');
+	rtn = ft_substr(line, 0, new_line - line + 1);
+	free(line);
+	line  = ft_substr(new_line,1, ft_strlen(new_line));
 	return (rtn);
-
 }
 
-int main(void)
-{
 
-	char *s1 = "Hello";
-	char *s2 = ft_substr(s1, 1,3);
-	printf("This is s1[%s] this is s2[%s]", s1,s2);
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			printf("Can't open the file\n");
+		else
+			for(int i = 0; i  < 4; i++)
+			{
+				printf("This is my line %d \n------\n%s-------\n",i+1,get_next_line(fd));
+			}
+	}
+	else {
+	printf("Please provide 2 arguments\n");
+	}
 }
